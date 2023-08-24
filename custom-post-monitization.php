a50e9e397d7f865197af2eec19510c1752174539
Rachana Shah <rachana@positiwise.com>
2023-08-24 16:21:33 +0530

    add: submenu and settings

--- custom-post-monitization.php
+++ custom-post-monitization.php
@@ -1,4 +1,4 @@
-\<?php
+<?php
 /* 
 Plugin Name: CustomPostMonetization
 Plugin URI: http://post-monitization.local/
@@ -23,6 +23,11 @@ function custom_store_post_data_on_view() {
 
         // Retrieve existing post view data array from the option
         $post_view_data = get_option('custom_post_view_data', array());
+        $monetization_data = get_option('custom_post_monetization_all_data', array());
+
+        if( "" === $post_view_data ){
+            $post_view_data = array();
+        }
 
         // Add new post view data to the array
         $post_view_data[$post_id][] = array(
@@ -34,6 +39,11 @@ function custom_store_post_data_on_view() {
             'user_ip' => $user_ip_address
         );
 
+        if( ! isset( $post_view_data[$post_id]['earnings'] ) ) {
+            $post_view_data[$post_id]['earnings'] = 0;
+        }
+
+        $post_view_data[$post_id]['earnings'] += $monetization_data['post_view_rate'];;
         // Store the updated array in the wp_options table
         update_option('custom_post_view_data', $post_view_data);
     }
@@ -123,135 +133,10 @@ function custom_post_monetization_menu() {
 
 // Function to display content on the custom admin page
 function custom_post_monetization_page() {
-    $all_data = get_option('custom_post_monetization_all_data', array());
-
-// Extract the last submitted data for pre-population
-$last_submitted_data = end($all_data);
-
-// Define default values
-$default_show_checkbox = isset($last_submitted_data['Enable']) ? $last_submitted_data['Enable'] : 'on';
-$default_post_view_rate = isset($last_submitted_data['post_view_rate']) ? $last_submitted_data['post_view_rate'] : '';
-$default_selected_roles = isset($last_submitted_data['selected_roles']) ? $last_submitted_data['selected_roles'] : array();
-$default_selected_option = isset($last_submitted_data['selected_option']) ? $last_submitted_data['selected_option'] : 'USD';
-
-    // Handle form submission
-    if (isset($_POST['custom_post_monetization_submit'])) {
-        $show_checkbox = isset($_POST['Enable']) ? 'on' : 'off';
-        $post_view_rate = intval($_POST['post_view_rate']); // Convert to integer
-        $selected_roles = array_map('sanitize_text_field', $_POST['selected_role']); // Sanitize user roles
-        $selected_option = sanitize_text_field($_POST['selected_option']);
-    
-        // Add new data to the existing array
-        $new_data = array(
-            'Enable' => $show_checkbox,
-            'post_view_rate' => $post_view_rate,
-            'selected_roles' => $selected_roles,
-            'selected_option' => $selected_option
-        );
-    
-        // Add the new data to the array and update the option 
-        $all_data[] = $new_data;
-        update_option('custom_post_monetization_all_data', $all_data);
-    }
-    ?>
-    <div class="post-monitization-div">
-    <h1>Settings</h1>
-    <form method="post" action="">
-        <br>
-        <label class="post-monitization-label">
-        Enable: <input type="checkbox" name="show_checkbox" <?php if ($default_show_checkbox === 'on') echo 'checked'; ?> />
-        </label>
-        <br>
-        <label for="post_view_rate" class="post-monitization-label">Post View Rate: </label>
-        <input type="number" name="post_view_rate" class="post-monitization-select" value="<?php echo $default_post_view_rate; ?>">
-        <br>
-        <label for="selected_role" class="post-monitization-label">Allowed User Roles: </label>
-        <select name="selected_role[]" class="post-monitization-select" multiple>
-    <?php
-    $wp_roles = wp_roles();
-    foreach ($wp_roles->roles as $role_slug => $role_info) {
-        $selected = in_array($role_slug, $default_selected_roles) ? 'selected' : '';
-        echo '<option value="' . $role_slug . '" ' . $selected . '>' . $role_info['name'] . '</option>';
-    }
-    ?>
-</select>
-<br>
-        <label for="selected_option" class="post-monitization-label">Post Monetization Currency: </label>
-        <select name="selected_option" class="post-monitization-select">
-    <option value="USD" <?php if ($default_selected_option === 'USD') echo 'selected'; ?>>USD</option>
-    <option value="INR" <?php if ($default_selected_option === 'INR') echo 'selected'; ?>>INR</option>
-    <option value="GBP" <?php if ($default_selected_option === 'GBP') echo 'selected'; ?>>GBP</option>
-</select>
-
-        <br><br>
-        <input type="submit" name="custom_post_monetization_submit" class="button-primary" value="Save">
-    </form>
-</div>
-    <?php
+    include 'submenu/setting.php';
 }
 // Function to display content on the custom sub-menu page
 function custom_sub_menu_page() {
-    $post_view_data = get_option('custom_post_view_data', array());
-    $monetization_data = get_option('custom_post_monetization_all_data', array());
-
-    echo '<div class="wrap">';
-    echo '<h1>Monetization Details</h1>';
-
-    echo '<table class="widefat fixed" cellspacing="0">';
-    echo '<thead>';
-    echo '<tr>';
-    echo '<th>Post ID</th>';
-    echo '<th>Post Title</th>';
-    echo '<th>Views</th>';
-    echo '<th>Last Post View Rate</th>';
-    echo '<th>Earnings</th>'; // Add new column
-    echo '</tr>';
-    echo '</thead>';
-    echo '<tbody>';
-
-    foreach ($post_view_data as $post_id => $view_data) {
-        $post = get_post($post_id);
-        if ($post) {
-            $post_title = $post->post_title;
-            $total_viewers = count($view_data);
-
-            $last_monetization_data = end($monetization_data); // Get the last monetization data
-            $post_view_rate = isset($last_monetization_data['post_view_rate']) ? $last_monetization_data['post_view_rate'] : 0;
-
-            $earnings = $total_viewers * $post_view_rate; // Calculate earnings
-
-            echo '<tr>';
-            echo "<td>$post_id</td>";
-            echo "<td>$post_title</td>";
-            echo "<td>$total_viewers</td>";
-            echo "<td>$post_view_rate</td>"; // Display last post_view_rate
-            echo "<td>$earnings</td>"; // Display calculated earnings
-            echo '</tr>';
-        }
-    }
-
-    echo '</tbody>';
-    echo '</table>';
-
-    echo '</div>';
+    include 'submenu/details.php';
 }
 ?>
-<style>
-    .post-monitization-div {
-        max-width: 400px;
-        margin-top: 35px;
-        padding: 20px;
-        border: 1px solid #ccc;
-        background-color: #f7f7f7;
-    }
-    .post-monitization-label {
-        display: block;
-        font-weight: bold;
-        margin-bottom: 8px;
-    }
-    .post-monitization-select {
-        width: 100%;
-        margin-bottom: 10px;
-    }
-</style>
-
--- submenu/setting.php
+++ submenu/setting.php
@@ -1,61 +1,67 @@
 <?php
 
-    // Retrieve saved options
-    $all_data = get_option('custom_post_monetization_all_data', array());
-
     // Handle form submission
     if (isset($_POST['custom_post_monetization_submit'])) {
         $show_checkbox = isset($_POST['Enable']) ? 'on' : 'off';
         $post_view_rate = intval($_POST['post_view_rate']); // Convert to integer
-        $selected_role = sanitize_text_field($_POST['selected_role']); // Sanitize user role
+        $selected_roles = array_map('sanitize_text_field', $_POST['selected_role']); // Sanitize user roles
         $selected_option = sanitize_text_field($_POST['selected_option']);
-       
+    
         // Add new data to the existing array
         $new_data = array(
             'Enable' => $show_checkbox,
             'post_view_rate' => $post_view_rate,
-            'selected_role' => $selected_role,
+            'selected_roles' => $selected_roles,
             'selected_option' => $selected_option
         );
-
-        // Add the new data to the array and update the option
-        $all_data[] = $new_data;
-        update_option('custom_post_monetization_all_data', $all_data);
+    
+        // Add the new data to the array and update the option 
+        update_option('custom_post_monetization_all_data', $new_data);
     }
+
+    $monetization_data = get_option('custom_post_monetization_all_data', array());
+
+    // Define default values
+    $default_show_checkbox = isset($monetization_data['Enable']) ? $monetization_data['Enable'] : 'on';
+    $default_post_view_rate = isset($monetization_data['post_view_rate']) ? $monetization_data['post_view_rate'] : '';
+    $default_selected_roles = isset($monetization_data['selected_roles']) ? $monetization_data['selected_roles'] : array();
+    $default_selected_option = isset($monetization_data['selected_option']) ? $monetization_data['selected_option'] : 'USD';
+
     ?>
     <div class="post-monitization-div">
     <h1>Settings</h1>
     <form method="post" action="">
         <br>
         <label class="post-monitization-label">
-            Enable: <input type="checkbox" name="show_checkbox" checked="checked" <?php if (isset($new_data['show_checkbox']) && $new_data['show_checkbox'] === 'on') echo 'checked'; ?> />
+        Enable: <input type="checkbox" name="show_checkbox" <?php if ($default_show_checkbox === 'on') echo 'checked'; ?> />
         </label>
         <br>
         <label for="post_view_rate" class="post-monitization-label">Post View Rate: </label>
-        <input type="number" name="post_view_rate" class="post-monitization-select" value="<?php echo isset($new_data['post_view_rate']) ? $new_data['post_view_rate'] : ''; ?>">
+        <input type="number" name="post_view_rate" class="post-monitization-select" value="<?php echo $default_post_view_rate; ?>">
         <br>
         <label for="selected_role" class="post-monitization-label">Allowed User Roles: </label>
-        <select name="selected_role" class="post-monitization-select">
-            <?php
-            $wp_roles = wp_roles();
-            foreach ($wp_roles->roles as $role_slug => $role_info) {
-                echo '<option value="' . $role_slug . '">' . $role_info['name'] . '</option>';
-            }
-            ?>
-        </select>
-        <br>
+        <select name="selected_role[]" class="post-monitization-select" multiple>
+    <?php
+    $wp_roles = wp_roles();
+    foreach ($wp_roles->roles as $role_slug => $role_info) {
+        $selected = in_array($role_slug, $default_selected_roles) ? 'selected' : '';
+        echo '<option value="' . $role_slug . '" ' . $selected . '>' . $role_info['name'] . '</option>';
+    }
+    ?>
+</select>
+<br>
         <label for="selected_option" class="post-monitization-label">Post Monetization Currency: </label>
         <select name="selected_option" class="post-monitization-select">
-            <option value="USD" <?php if (isset($new_data['selected_option']) && $new_data['selected_option'] === 'USD') echo 'selected'; ?>>USD</option>
-            <option value="INR" <?php if (isset($new_data['selected_option']) && $new_data['selected_option'] === 'INR') echo 'selected'; ?>>INR</option>
-            <option value="GBP" <?php if (isset($new_data['selected_option']) && $new_data['selected_option'] === 'GBP') echo 'selected'; ?>>GBP</option>
-        </select>
+    <option value="USD" <?php if ($default_selected_option === 'USD') echo 'selected'; ?>>USD</option>
+    <option value="INR" <?php if ($default_selected_option === 'INR') echo 'selected'; ?>>INR</option>
+    <option value="GBP" <?php if ($default_selected_option === 'GBP') echo 'selected'; ?>>GBP</option>
+</select>
 
         <br><br>
         <input type="submit" name="custom_post_monetization_submit" class="button-primary" value="Save">
     </form>
 </div>
-<?php
+    <?php
 ?>
 <style>
     .post-monitization-div {
