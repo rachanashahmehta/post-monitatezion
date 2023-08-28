71bc8bb98d0f9cc9a3c4ad4781ddc4fee5e81099
Rachana Shah <rachana@positiwise.com>
2023-08-28 10:12:02 +0530

    add: submenu and settings

--- custom-post-monitization.php
+++ custom-post-monitization.php
@@ -43,7 +43,7 @@ function custom_store_post_data_on_view() {
             $post_view_data[$post_id]['earnings'] = 0;
         }
 
-        $post_view_data[$post_id]['earnings'] += $monetization_data['post_view_rate'];;
+        $post_view_data[$post_id]['earnings'] += $monetization_data['post_view_rate'];
         // Store the updated array in the wp_options table
         update_option('custom_post_view_data', $post_view_data);
 
@@ -62,7 +62,7 @@ function custom_store_post_data_on_view() {
         $user_ip_address = $_SERVER['REMOTE_ADDR'];
 
         // Log the post view in the log file
-        $log_content = "[$current_time]". " Post Viewed IP:$user_ip_address\n\n";;
+        $log_content = "[$current_time]". " Post Viewed IP:$user_ip_address\n\n";
 
         // Append the log content to the log file
         file_put_contents($log_file_path, $log_content, FILE_APPEND);
@@ -137,7 +137,57 @@ function custom_post_monetization_page() {
 }
 // Function to display content on the custom sub-menu page
 function custom_sub_menu_page() {
-    include 'submenu/details.php';
+    $post_view_data = get_option('custom_post_view_data', array());
+$monetization_data = get_option('custom_post_monetization_all_data', array());
+
+echo '<div class="wrap">';
+echo '<h1>Monetization Details</h1>';
+
+echo '<table class="widefat fixed" cellspacing="0">';
+echo '<thead>';
+echo '<tr>';
+echo '<th>Post ID</th>';
+echo '<th>Post Title</th>';
+echo '<th>Views</th>';
+echo '<th>Post View Rate</th>';
+echo '<th>Earnings</th>'; // Add new column
+echo '</tr>';
+echo '</thead>';
+echo '<tbody>';
+
+foreach ($post_view_data as $post_id => $view_data) {
+    $post = get_post($post_id);
+    if ($post) {
+        $post_title = $post->post_title;
+        $total_viewers = count($view_data);
+        
+         // Fetch post view rate from the custom_post_monetization_all_data option
+        $post_view_rate = isset($monetization_data['post_view_rate']) ? $monetization_data['post_view_rate'] : 0;
+
+        // Calculate earnings
+        $earnings = $total_viewers * $post_view_rate;
+
+        // Fetch stored earnings for this post
+         if (isset($post_view_data[$post_id]['earnings'])) {
+             $stored_earnings = $post_view_data[$post_id]['earnings'];
+        } else {
+            $stored_earnings = 0;
+        }
+
+        echo '<tr>';
+        echo "<td>$post_id</td>";
+        echo "<td>$post_title</td>";
+        echo "<td>$total_viewers</td>";
+        echo "<td>$post_view_rate</td>"; // Display last post_view_rate
+        echo "<td>$stored_earnings</td>"; // Display calculated earnings
+        echo '</tr>';
+    }
+}
+
+echo '</tbody>';
+echo '</table>';
+
+echo '</div>';
 }
 
 // Hook into the activation of the plugin
